5e81b344c307f948089b1bd87bbfcf227414b059
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-09-21 14:23:16 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,42 @@ pub struct Superlemma {
     ortografi: String,
     ordklass: String,
     #[serde(rename="SOLemman")]
-    so_lemman: Vec<serde_json::Value>,
+    so_lemman: Vec<SoLemma>,
     #[serde(rename="SAOLLemman")]
-    saol_lemman: Vec<serde_json::Value>,
+    saol_lemman: Vec<SaolLemma>,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+pub struct SoLemma {
+    ortografi: String,
+    lm_sabob: u32,
+    visas: bool,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+pub struct SaolLemma {
+    ortografi: String,
+    #[serde(rename="homografNr")]
+    homograf_nr: Option<HomografNr>,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+pub struct HomografNr {
+    nummer: String,
+    version: String
 }
 
 #[derive(serde::Serialize)]
-struct Row<'a> {
+struct OrtografiOrdklassRow<'a> {
     ortografi: &'a str,
     ordklass: &'a str,
-    // Serde allows us to name our headers exactly,
-    // even if they don't match our struct field names.
+    num_saol_lemman: usize,
+    num_so_lemman: usize,
+}
+
+#[derive(serde::Serialize)]
+struct OrtografiRow<'a> {
+    ortografi: &'a str,
     num_saol_lemman: usize,
     num_so_lemman: usize,
 }
@@ -50,36 +75,73 @@ fn try_main() -> Result<(), Error> {
             log::trace!("command 'extract'");
             let input = submatches.get_one::<PathBuf>("input").expect("`input` is required");
             log::debug!("reading from '{}'", input.display());
-            let output = submatches.get_one::<PathBuf>("output").expect("`output` is required");
-            log::debug!("writing to '{}'", output.display());
+            let output_stub: &String = submatches.get_one("output_stub").expect("`outputstub` is required");
             let fp_in = fs::File::open(input)?;
             let gz_in = GzDecoder::new(fp_in);
             let reader = io::BufReader::new(gz_in);
             let mut read_line_count = 0;
-            let mut write_line_count = 0;
-            let mut lemmas: HashMap<(String, String), (usize, usize)> = HashMap::new();
-            let mut writer = csv::WriterBuilder::new()
-                .has_headers(true)
-                .from_path(output)?;
+            let mut lemmas_by_orto_ordk: HashMap<(String, String), (usize, usize)> = HashMap::new();
+            let mut lemmas_by_orto: HashMap<String, (usize, usize)> = HashMap::new();
+
             for line in reader.lines() {
                 read_line_count += 1;
                 let entry: KarpImportEntry = serde_json::from_str(&line?)?;
 
-                let (num_saol_lemman, num_so_lemman) = lemmas.entry((entry.entry.ortografi, entry.entry.ordklass))
+                let sum_so_lemman = entry.entry.so_lemman
+                    .iter()
+                    .filter(|x| x.visas)
+                    .fold(0, |acc, _| acc + 1);
+                let (num_saol_lemman, num_so_lemman) = lemmas_by_orto_ordk.entry((entry.entry.ortografi.clone(), entry.entry.ordklass))
                     .or_insert((0, 0));
                 *num_saol_lemman += entry.entry.saol_lemman.len();
-                *num_so_lemman += entry.entry.so_lemman.len();
+                *num_so_lemman += sum_so_lemman;
+
+                let (num_saol_lemman, num_so_lemman) = lemmas_by_orto.entry(entry.entry.ortografi)
+                    .or_insert((0, 0));
+                *num_saol_lemman += entry.entry.saol_lemman.len();
+                *num_so_lemman += sum_so_lemman;
 
             }
             log::info!("Line read: {}", read_line_count);
-            for ((ortografi, ordklass), (num_saol_lemman, num_so_lemman)) in &lemmas {
+
+            log::info!("Writing lemmas by ortografi and ordklass ...");
+            let orto_ordk_path = format!("{}.ortografi.ordklass.csv", output_stub);
+            log::debug!("writing to '{}'", orto_ordk_path);
+            let mut orto_ordk_writer = csv::WriterBuilder::new()
+                .has_headers(true)
+                .from_path(orto_ordk_path)?;
+            let mut write_line_count = 0;
+            for ((ortografi, ordklass), (num_saol_lemman, num_so_lemman)) in &lemmas_by_orto_ordk {
                 if num_saol_lemman == &0usize || num_so_lemman == &0usize {
                     continue;
                 }
                 if num_saol_lemman != num_so_lemman {
-                    writer.serialize(Row {
+                    orto_ordk_writer.serialize(OrtografiOrdklassRow {
                         ortografi,
                         ordklass,
+                        num_saol_lemman: *num_saol_lemman,
+                        num_so_lemman: *num_so_lemman,
+                    })?;
+                    write_line_count += 1;
+                }
+            }
+            log::info!("Line written: {}", write_line_count);
+
+            log::info!("Writing lemmas by ortografi ...");
+            let orto_path = format!("{}.ortografi.csv", output_stub);
+            log::debug!("writing to '{}'", orto_path);
+
+            let mut orto_writer = csv::WriterBuilder::new()
+                .has_headers(true)
+                .from_path(orto_path)?;
+            let mut write_line_count = 0;
+            for (ortografi, (num_saol_lemman, num_so_lemman)) in &lemmas_by_orto {
+                if num_saol_lemman == &0usize || num_so_lemman == &0usize {
+                    continue;
+                }
+                if num_saol_lemman != num_so_lemman {
+                    orto_writer.serialize(OrtografiRow {
+                        ortografi,
                         num_saol_lemman: *num_saol_lemman,
                         num_so_lemman: *num_so_lemman,
                     })?;
@@ -115,10 +177,10 @@ fn cli() -> clap::Command<'static> {
                         .required(true)
                 )
                 .arg(
-                    Arg::new("output")
+                    Arg::new("output_stub")
                         .takes_value(true)
-                        .help("csv file to write")
-                        .value_parser(clap::value_parser!(PathBuf))
+                        .help("file_stub to write")
+                        // .value_parser(clap::value_parser!(PathBuf))
                         .required(true)
                 )
         )

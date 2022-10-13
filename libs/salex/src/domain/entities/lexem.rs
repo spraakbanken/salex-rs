e2dcb8b8307bdf9743911880db1768a16b093095
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-10-13 08:24:12 +0200

    fix: hv

--- libs/salex/src/domain/entities/lexem.rs
+++ libs/salex/src/domain/entities/lexem.rs
@@ -29,10 +29,14 @@ pub struct Ämnesområde {
 }
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
-#[serde(rename_all = "camelCase", deny_unknown_fields)]
+#[serde(deny_unknown_fields)]
 pub struct Hv {
     typ: HänvisningsTyp,
     hänvisning: String,
+    kommentar: String,
+    visas: bool,
+    l_nr: u32,
+    kc_nr: u32,
 }
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
@@ -40,6 +44,20 @@ pub struct Hv {
 pub enum HänvisningsTyp {
     #[serde(rename = "SE:se")]
     Se,
+    #[serde(rename = "JFR:jämför")]
+    Jämför,
+    #[serde(rename = "JFR:hyperonym")]
+    Hyperonym,
+    #[serde(rename = "JFR:hyponym")]
+    Hyponym,
+    #[serde(rename = "MOTSATS:antonym")]
+    Antonym,
+    #[serde(rename = "SYN:synonym")]
+    Synonym,
+    #[serde(rename = "JFR:cohyponym")]
+    Cohyponym,
+    #[serde(rename = "SE ÄVEN:av-/härledning")]
+    Avledning,
 }
 
 #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]

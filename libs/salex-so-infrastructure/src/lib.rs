7f14df342da4055b01837b6c09f5639aa9953de2
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-10-13 12:50:04 +0200

    fix: add mysql feature

--- libs/salex-so-infrastructure/src/lib.rs
+++ libs/salex-so-infrastructure/src/lib.rs
@@ -2,6 +2,13 @@ pub mod configuration;
 pub mod infrastructure;
 pub mod telemetry;
 
+#[cfg(feature = "mysql")]
+pub use sqlx::mysql::{MySqlPool as DbPool, MySqlRow as DbRow};
+
+#[cfg(feature = "mysql")]
+pub async fn connect(db_url: &str) -> Result<DbPool, sqlx::Error> {
+    DbPool::connect(db_url).await
+}
 #[cfg(feature = "sqlite")]
 pub use sqlx::sqlite::{SqlitePool as DbPool, SqliteRow as DbRow};
 
